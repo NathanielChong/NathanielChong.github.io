9d68286792f1fa336cf6c405477c1ae88af4e6a5
Nathaniel Chong <1natechong@gmail.com>
2021-09-14 02:52:05 -0400

    Change grammar

--- src/components/infosnippet.js
+++ src/components/infosnippet.js
@@ -6,7 +6,7 @@ function InfoSnippet() {
   return (
        <div id="info" className="centered">
        <h2>Hi! I'm Nathan. Right now I'm in: <a href="https://www.a2gov.org/Pages/default.aspx" target="_blank" color="green" className="link">Ann Arbor</a></h2> 
-       <h3> I'm a senior at the <a href="https://www.eecs.umich.edu/" target="_blank" className="link">University of Michigan and soon to be graduate student</a> </h3>
+       <h3> I'm a senior and soon to be graduate student at the <a href="https://www.eecs.umich.edu/" target="_blank" className="link">University of Michigan</a> </h3>
         <h3>Say hello at: <a href="mailto:nychong@umich.edu" className="link">nychong@umich.edu</a>!</h3>
       
        </div>
