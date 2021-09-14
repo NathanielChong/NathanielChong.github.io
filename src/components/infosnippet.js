b02f99e9f17a04edb4ed794f42e5d31dc02fcc63
Nathaniel Chong <1natechong@gmail.com>
2021-09-14 13:28:17 -0400

    Icon bar implemented

--- src/components/infosnippet.js
+++ src/components/infosnippet.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import '../App.css';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab, fas} from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import 'font-awesome/css/font-awesome.min.css';
 
 
 function InfoSnippet() {
@@ -8,7 +12,19 @@ function InfoSnippet() {
        <h2>Hi! I'm Nathan. Right now I'm in: <a href="https://www.a2gov.org/Pages/default.aspx" target="_blank" color="green" className="link">Ann Arbor</a></h2> 
        <h3> I'm a senior and soon to be graduate student at the <a href="https://www.eecs.umich.edu/" target="_blank" className="link">University of Michigan</a> </h3>
         <h3>Say hello at: <a href="mailto:nychong@umich.edu" className="link">nychong@umich.edu</a>!</h3>
-      
+
+        <div id="icons">
+        <a href="https://www.linkedin.com/in/nathanielychong/"> 
+        <i class="fa fa-linkedin fa-3x" aria-hidden="true"></i> 
+         </a>
+        
+        <a class="nonMidIcon" href="https://github.com/NathanielChong"> 
+        <i class="fa fa-github fa-3x" aria-hidden="true"></i> </a>
+
+        <a class="nonMidIcon" href="mailto:nychong@umich.edu"><i class="fa fa-envelope-square fa-3x"></i> 
+        </a>
+
+        </div>
        </div>
   )
 }
