3ee12029b220a07e04615a41bc8087a8a31e2ffd
Nathaniel Chong <1natechong@gmail.com>
2022-08-06 16:54:19 -0700

    Update for 2022

--- src/components/infosnippet.js
+++ src/components/infosnippet.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import '../App.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab, fas} from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import 'font-awesome/css/font-awesome.min.css';
 
 
 function InfoSnippet() {
   return (
-       <div id="info" className="centered">
-       <h2>Hi! I'm Nathan. Right now I'm in: <a href="https://www.a2gov.org/Pages/default.aspx" target="_blank" color="green" className="link">Ann Arbor</a></h2> 
-       <h3> I'm a senior and soon to be graduate student at the <a href="https://www.eecs.umich.edu/" target="_blank" className="link">University of Michigan</a> </h3>
-        <h3>Say hello at: <a href="mailto:nychong@umich.edu" className="link">nychong@umich.edu</a>!</h3>
+    <div id="info" className="centered">
+      <h2>Hi! I'm Nathan. Right now I'm in: <a href="https://www.a2gov.org/Pages/default.aspx" target="_blank" color="green" className="link">Ann Arbor</a></h2>
+      <h3> I'm a graduate student at the <a href="https://www.eecs.umich.edu/" target="_blank" className="link">University of Michigan</a> </h3>
+      <h3>Say hello at: <a href="mailto:nychong@umich.edu" className="link">nychong@umich.edu</a>!</h3>
 
-        <div id="icons">
-        <a href="https://www.linkedin.com/in/nathanielychong/"> 
-        <i class="fa fa-linkedin fa-3x" aria-hidden="true"></i> 
-         </a>
-        
-        <a class="nonMidIcon" href="https://github.com/NathanielChong"> 
-        <i class="fa fa-github fa-3x" aria-hidden="true"></i> </a>
-
-        <a class="nonMidIcon" href="mailto:nychong@umich.edu"><i class="fa fa-envelope-square fa-3x"></i> 
+      <div id="icons">
+        <a href="https://www.linkedin.com/in/nathanielychong/">
+          <i class="fa fa-linkedin fa-3x" aria-hidden="true"></i>
         </a>
 
-        </div>
-       </div>
+        <a class="nonMidIcon" href="https://github.com/NathanielChong">
+          <i class="fa fa-github fa-3x" aria-hidden="true"></i></a>
+
+        <a class="nonMidIcon" href="mailto:nychong@umich.edu"><i class="fa fa-envelope-square fa-3x"></i>
+        </a>
+
+      </div>
+    </div>
   )
 }
 
--- src/components/redirectionbar.js
+++ src/components/redirectionbar.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import '../App.css';
-import { Navbar, NavItem, NavDropdown, MenuItem, Nav, Container } from 'react-bootstrap';
+import { Navbar, Nav, Container } from 'react-bootstrap';
 
 function RedirectionBar() {
   return (
-       <div id="navbar wrapper">
-       	<Navbar>
-          <Container>
-       		<Nav.Link href="./index.html" style={{ color: 'black' }} className="NavItem"> Home </Nav.Link>
-       		<Nav.Link href="./NathanielChongResume.pdf" style={{ color: 'black' }} className="NavItem"> Resume </Nav.Link> 
-       		<Nav.Link href="./projects.html" style={{ color: 'black' }} className="NavItem"> Projects/Research </Nav.Link>
-       		<Nav.Link href="./contactme.html" style={{ color: 'black' }} className="NavItem"> Contact </Nav.Link>
+    <div id="navbar wrapper">
+      <Navbar>
+        <Container>
+          <Nav.Link href="./index.html" style={{ color: 'black' }} className="NavItem"> Home </Nav.Link>
+          <Nav.Link href="./NathanielYChongResume2022.pdf" style={{ color: 'black' }} className="NavItem"> Resume </Nav.Link>
+          <Nav.Link href="./projects.html" style={{ color: 'black' }} className="NavItem"> Projects/Research </Nav.Link>
+          <Nav.Link href="./contactme.html" style={{ color: 'black' }} className="NavItem"> Contact </Nav.Link>
         </Container>
-       	</Navbar>
-       </div>
+      </Navbar>
+    </div>
   )
 }
 
