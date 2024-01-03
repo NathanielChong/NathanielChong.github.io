3641774bd23b092a565f8dea80c1aaebaffdda1b
Nathaniel Chong <1natechong@gmail.com>
2024-01-03 00:21:38 -0500

    Update info for swe

--- src/components/infosnippet.js
+++ src/components/infosnippet.js
@@ -6,8 +6,8 @@ import 'font-awesome/css/font-awesome.min.css';
 function InfoSnippet() {
   return (
     <div id="info" className="centered">
-      <h2>Hi! I'm Nathan. Right now I'm in: <a href="https://www.a2gov.org/Pages/default.aspx" target="_blank" color="green" className="link">Ann Arbor</a></h2>
-      <h3> I'm a graduate student at the <a href="https://www.eecs.umich.edu/" target="_blank" className="link">University of Michigan</a> </h3>
+      <h2>Hi! I'm Nathan. Right now I'm in: <a href="https://visitseattle.org/" target="_blank" color="green" className="link">Seattle</a></h2>
+      <h3> I'm a software engineer at <a href="https://www.metacareers.com/" target="_blank" className="link">Meta</a> </h3>
       <h3>Say hello at: <a href="mailto:nychong@umich.edu" className="link">nychong@umich.edu</a>!</h3>
 
       <div id="icons">
